beddeb99ff4f14c0580ac73a01987cbeec3fc87c
Fiona Lapham <fionadlapham@gmail.com>
2018-05-09 11:47:11 +0000

    first_choice method, deleted restaurant_list method

--- lib/michelin3star/cli.rb
+++ lib/michelin3star/cli.rb
@@ -3,11 +3,21 @@ require 'pry'
 class CLI
 
   def call
-    puts"Hello, would you like to check out some three star Michelin restaurants? You've come to the right place."
-    puts "Would you like to list all of the Michelin 3 Star Michelin Restaurants in the World? y/n"
+    puts"Hello, would you like to check out some three star Michelin restaurants? If your answer is yes, you've come to the right place!"
+    puts "If you would like to see the names of all the Michelin Three Star restaurants in the world type 1. If you prefer to see a list of countries where Michelin 3 star restaurants exist type 2."
     restaurant_list
     binding.pry
     end
+
+  def first_choice
+    input = gets.strip
+    case input
+    when "1"
+      Restaurants.all.name
+    when "2"
+      Country.all.name
+    end
+  end
 
   def restaurant_list
     input = gets.strip
