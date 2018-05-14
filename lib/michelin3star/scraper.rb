02b76db1dd74ef73d152cd14521b57add091a308
Fiona Lapham <fionadlapham@gmail.com>
2018-05-14 19:50:36 +0000

    update to get_country_data (removes whitespace)

--- lib/michelin3star/scraper.rb
+++ lib/michelin3star/scraper.rb
@@ -12,8 +12,7 @@ class Scraper
 #use the "h3" to call all of the arrays of "h3" that should include the text with the name of the country.Figure out why it is also returning a bunch of " "
   def get_country_data
      get_page.css("h3").collect do |country|
-      country.css(" .mw-headline").text
-      binding.pry
+      country.css(" .mw-headline").text.lstrip
     end
   end
 
