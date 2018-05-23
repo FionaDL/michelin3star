eead73183c3e1722f28d09782b21f43598ebaa74
Fiona Lapham <fionadlapham@gmail.com>
2018-05-23 20:16:49 +0000

    moves requires to enviornment

--- lib/michelin3star.rb
+++ lib/michelin3star.rb
@@ -3,3 +3,7 @@ require_relative "./michelin3star/cli"
 require_relative "./michelin3star/restaurants"
 require_relative "./michelin3star/scraper"
 require_relative "./michelin3star/country"
+
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
